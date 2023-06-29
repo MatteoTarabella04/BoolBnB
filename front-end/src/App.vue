e84eaf42f2376e003bd037c469628ffcbcc3e3b8
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-29 17:14:51 +0200

    add front initial commit set stages

---
+++ front-end/src/App.vue
@@ -0,0 +1,10 @@
+<script>
+
+export default {
+    components: {}
+}
+</script>
+
+<template></template>
+
+<style lang="scss"></style>
---
+++ front-end/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles/general.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
