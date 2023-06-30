a6801d1552b428acc845a772166e814d6b628b6f
nicolaazonta <nicolazonta114@gmail.com>
2023-06-30 16:25:49 +0200

    fix v-model.trim

--- front-end/src/views/UserRegistrationView.vue
+++ front-end/src/views/UserRegistrationView.vue
@@ -53,35 +53,35 @@ export default {
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control" name="password" id="password"
                                 aria-describedby="passwordhelpId" placeholder="Inserisci la password" required
-                                v-model.trim="email">
+                                v-model.trim="password">
                         </div>
 
                         <div class="mb-4">
                             <label for="password_confirmation" class="form-label">Conferma la tua password</label>
                             <input type="password" class="form-control" name="password_confirmation"
                                 id="password_confirmation" aria-describedby="password_confirmationhelpId"
-                                placeholder="Inserisci nuovamente la password per conferma" required v-model.trim="email">
+                                placeholder="Inserisci nuovamente la password per conferma" required v-model.trim="password_confirmation">
                         </div>
 
                         <div class="mb-4">
                             <label for="first_name" class="form-label">Nome</label>
                             <input type="text" class="form-control" name="first_name" id="first_name"
                                 aria-describedby="first_namehelpId" placeholder="Inserisci il tuo nome" required
-                                v-model.trim="email">
+                                v-model.trim="first_name">
                         </div>
 
                         <div class="mb-4">
                             <label for="last_name" class="form-label">Cognome</label>
                             <input type="text" class="form-control" name="last_name" id="last_name"
                                 aria-describedby="last_namehelpId" placeholder="Inserisci il tuo cognome" required
-                                v-model.trim="email">
+                                v-model.trim="last_name">
                         </div>
 
                         <div class="mb-4">
                             <label for="date_of_birth" class="form-label">Data di nascita</label>
                             <input type="date" class="form-control" name="date_of_birth" id="date_of_birth"
                                 aria-describedby="date_of_birthhelpId" placeholder="Inserisci la tua data di nascita"
-                                required v-model.trim="email">
+                                required v-model.trim="date_of_birth">
                         </div>
 
                         <button type="submit" class="btn btn-primary w-100">CONFERMA</button>
