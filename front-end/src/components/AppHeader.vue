b5daf60da01d2805227634abf250e226560e37c5
nicolaazonta <nicolazonta114@gmail.com>
2023-06-30 19:54:57 +0200

    add some content in header

--- front-end/src/components/AppHeader.vue
+++ front-end/src/components/AppHeader.vue
@@ -1,8 +1,8 @@
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <template>
@@ -10,41 +10,82 @@
 
 
         <nav class="navbar navbar-expand-md navbar-light bg-light">
-              <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
+            <div class="container">
+                <router-link class="navbar-brand" :to="{ 'name': 'Home' }">BoolBnB</router-link>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+
                         <li class="nav-item">
-                            <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                            <router-link class="nav-link" class-active="" :to="{ 'name': 'UserLogin' }">
+                                <span>Accedi</span>
+                            </router-link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <router-link class="nav-link" class-active="" :to="{ 'name': 'UserRegistration' }">
+                                <span>Registrati</span>
+                            </router-link>
                         </li>
-                        <li class="nav-item dropdown">
+                        <li class="nav-item">
+                            <router-link class="nav-link" class-active="" :to="{ 'name': 'Home' }">
+                                <span>Esci</span>
+                            </router-link>
+                        </li>
+
+                        <!-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                             <div class="dropdown-menu" aria-labelledby="dropdownId">
                                 <a class="dropdown-item" href="#">Action 1</a>
                                 <a class="dropdown-item" href="#">Action 2</a>
                             </div>
-                        </li>
+                        </li> -->
                     </ul>
                     <!-- <form class="d-flex my-2 my-lg-0">
                         <input class="form-control me-sm-2" type="text" placeholder="Search">
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form> -->
                 </div>
-          </div>
+            </div>
         </nav>
-        
+
 
 
     </header>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@use '../styles/partials/colors.scss' as *;
 
+header {
+
+    & .navbar {
+        background-color: $bg-primary !important;
+    }
+
+    & .nav-link {
+        color: $text-primary !important;
+    }
+
+    & .nav-item {
+        border-radius: .5rem;
+        transition-property: all;
+        transition-duration: .4s;
+    }
+
+    & .nav-item:hover {
+        background-color: $bg-primary-hover !important;
+    }
+}
+
+body {
+    background-color: $bg-secondary !important;
+
+    & .card {
+        background-color: $bg-special-light !important;
+    }
+}
 </style>
