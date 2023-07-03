a1ffd266e29681384940c6366d639f8ec7e5899b
nicolaazonta <nicolazonta114@gmail.com>
2023-07-03 09:16:28 +0200

    add some bootstrap in create apartment

--- back-end/resources/views/admin/apartments/create.blade.php
+++ back-end/resources/views/admin/apartments/create.blade.php
@@ -1,96 +1,129 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card p-3 my-5 ">
+        @if (session('message'))
+            <div class="alert alert-danger" role="alert">
+                <strong>{{ session('message') }}</strong>
+            </div>
+        @endif
+      
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="alert alert-danger" role="alert">
+                    <strong>{{ $error }}</strong>
+                </div>
+            @endforeach
+        @endif
+      
+        <div>
+            <h4>Inserisci un annuncio!</h4>
+            <form action="{{ route('admin.apartments.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
 
-@if(session("message"))
-<div class="alert alert-danger" role="alert">
-  <strong>{{ session("message") }}</strong>
-</div>
-@endif
+                <div class="mb-3">
+                  <label for="image" class="form-label">Aggiungi un'immagine</label>
+                  <input type="file"
+                    class="form-control" name="image" id="image" aria-describedby="helpId">
+                    @error('image')
+                        <small class="text-danger">Per favore, inserisci correttamente l'immagine.</small>
+                    @enderror
+                </div>
 
-@if($errors->any())
-@foreach($errors->all() as $error)
-<div class="alert alert-danger" role="alert">
-  <strong>{{$error}}</strong>
-</div>
-@endforeach
-@endif
 
-<div>
-  <h4>Inserisci un annuncio!</h4>
-  <form action="{{ route('admin.apartments.store') }}" method="post" enctype="multipart/form-data">
-  @csrf
-    <div>
-      <label for="image">Aggiungi un immagine:</label>
-      <input type="file" id="image" accept="image/*" name="image" required>
-      @error('image')
-      <small class="text-danger">Per favore, inserisci correttamente l'immagine.</small>
-      @enderror
+                <div class="mb-3">
+                  <label for="" class="form-label">Nome</label>
+                  <input type="text"
+                    class="form-control" name="name" id="name" aria-describedby="helpId" placeholder="" value="{{ old('name') }}" required>
+                  @error('name')
+                      <small class="text-danger">Per favore, inserisci correttamente il nome.</small>
+                  @enderror
+                </div>
+                
+                <div class="mb-3">
+                  <label for="description" class="form-label">Descrizione</label>
+                  <textarea class="form-control" name="description" id="description" required rows="3">{{ old('description') }}</textarea>
+                  @error('description')
+                      <small class="text-danger">Per favore, inserisci correttamente la descrizione.</small>
+                  @enderror
+                </div>
+
+                <div class="mb-3">
+                  <label for="price_per_night" class="form-label">Prezzo a notte (€)</label>
+                  <input type="number"
+                    class="form-control" name="price_per_night" id="price_per_night" aria-describedby="helpId" placeholder="" value="{{ old('price_per_night') }}" required>
+                    @error('price_per_night')
+                        <small class="text-danger">Per favore, inserisci correttamente il prezzo per notte.</small>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                  <label for="rooms" class="form-label">Numero di stanze</label>
+                  <input type="number"
+                    class="form-control" name="rooms" id="rooms" aria-describedby="helpId" placeholder="" value="{{ old('rooms') }}" required>
+                    @error('rooms')
+                        <small class="text-danger">Per favore, inserisci correttamente il numero delle stanze.</small>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                  <label for="beds" class="form-label">Numero di letti</label>
+                  <input type="number"
+                    class="form-control" name="beds" id="beds" aria-describedby="helpId" placeholder="" value="{{ old('beds') }}" required>
+                    @error('beds')
+                        <small class="text-danger">Per favore, inserisci correttamente il numero di posti letto.</small>
+                    @enderror
+                </div>             
+        
+                <div class="mb-3">
+                  <label for="bathrooms" class="form-label">Numero di bagni</label>
+                  <input type="number"
+                    class="form-control" name="bathrooms" id="bathrooms" aria-describedby="helpId" placeholder="" value="{{ old('bathrooms') }}" required>
+                    @error('bathrooms')
+                        <small class="text-danger">Per favore, inserisci correttamente il numero di bagni.</small>
+                    @enderror
+                </div>               
+
+                <div class="mb-3">
+                  <label for="square_meters" class="form-label">Metri quadri</label>
+                  <input type="number"
+                    class="form-control" name="square_meters" id="square_meters" aria-describedby="helpId" placeholder="" value="{{ old('square_meters') }}" required>
+                    @error('square_meters')
+                        <small class="text-danger">Per favore, inserisci correttamente il numero di mq.</small>
+                    @enderror
+                </div>
+
+
+                <div class="mb-3">
+                  <label for="address" class="form-label">Indirizzo</label>
+                  <input type="text"
+                    class="form-control" name="address" id="address" aria-describedby="helpId" placeholder="" value="{{ old('address') }}" required>
+                  @error('address')
+                      <small class="text-danger">Per favore, inserisci correttamente l'indirizzo.</small>
+                  @enderror
+                </div>
+          
+                
+                <div class="form-check mb-3">
+                  <input class="form-check-input" type="checkbox" value="1" id="visible" checked>
+                  <label class="form-check-label" for="visible">
+                    Appartamento disponibile da subito
+                  </label>
+                  @error('visible')
+                      <small class="text-danger">Per favore, indica correttamente la visibilità.</small>
+                  @enderror
+                </div>
+
+
+                <button type="submit" class="btn btn-primary w-100 text-uppercase">Aggiungi appartamento</button>
+            </form>
+        </div>
+
+      </div>
     </div>
-    <div>
-      <label for="name">Nome:</label>
-      <input type="text" id="name" name="name" value="{{ old('name') }}" required>
-      @error('name')
-      <small class="text-danger">Per favore, inserisci correttamente il nome.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="description">Descrizione:</label>
-      <textarea id="description" name="description" required>{{ old('description') }}</textarea>
-      @error('description')
-      <small class="text-danger">Per favore, inserisci correttamente la descrizione.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="price_per_night">Prezzo a notte (€):</label>
-      <input type="number" id="price_per_night" name="price_per_night" value="{{ old('price_per_night') }}" required>
-      @error('price_per_night')
-      <small class="text-danger">Per favore, inserisci correttamente il prezzo per notte.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="rooms">Numero di stanze:</label>
-      <input type="number" id="rooms" name="rooms" value="{{ old('rooms') }}" required>
-      @error('rooms')
-      <small class="text-danger">Per favore, inserisci correttamente il numero delle stanze.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="beds">Numero di letti:</label>
-      <input type="number" id="beds" name="beds" value="{{ old('beds') }}" required>
-      @error('beds')
-      <small class="text-danger">Per favore, inserisci correttamente il numero di posti letto.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="bathrooms">Bagni:</label>
-      <input type="number" id="bathrooms" name="bathrooms" value="{{ old('bathrooms') }}" required>
-      @error('bathrooms')
-      <small class="text-danger">Per favore, inserisci correttamente il numero di bagni.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="square_meters">Dimensione (mq):</label>
-      <input type="number" id="square_meters" name="square_meters" value="{{ old('square_meters') }}" required>
-      @error('square_meters')
-      <small class="text-danger">Per favore, inserisci correttamente il numero di mq.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="address">Indirizzo:</label>
-      <input type="text" id="address" name="address" value="{{ old('address') }}" required>
-      @error('address')
-      <small class="text-danger">Per favore, inserisci correttamente l'indirizzo.</small>
-      @enderror
-    </div>
-    <div>
-      <label for="visible">Quest'appartamento è disponibile al pernottamento?:</label>
-      <input type="checkbox" id="visible" name="visible" value="1" required>
-      @error('visible')
-      <small class="text-danger">Per favore, indica correttamente la visibilità.</small>
-      @enderror
-    </div>
-    <button class="btn btn-primary" type="submit">Aggiungi appartamento</button>
-  </form>
+  </div>
 </div>
 @endsection
--- back-end/resources/views/partials/AppHeader.blade.php
+++ back-end/resources/views/partials/AppHeader.blade.php
@@ -1,52 +1,56 @@
 <header>
-  <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <div class="container">
-      <a href="{{ route('admin.apartments.index') }}">BoolBnB</a>
-      {{-- config('app.name', 'Laravel') --}}
-      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-          @guest
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
-          </li>
-          @if (Route::has('register'))
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
-          </li>
-          @endif
-          @else
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              {{ Auth::user()->first_name . " " . Auth::user()->last_name }}
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{__('Dashboard')}}</a>
-              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container">
+            <a class="text-decoration-none fw-bold fs-2 text-dark" href="{{ route('admin.apartments.index') }}">BoolBnB</a>
+            {{-- config('app.name', 'Laravel') --}}
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->first_name . ' ' . Auth::user()->last_name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
-                {{ __('Esci') }}
-              </a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-              </form>
-            </div>
-          </li>
-          @endguest
-          <!-- <li class="nav-item dropdown">
+                                    {{ __('Esci') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                    <!-- <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
                     <a class="dropdown-item" href="#">Action 1</a>
                     <a class="dropdown-item" href="#">Action 2</a>
                 </div>
             </li> -->
-        </ul>
-        <!-- <form class="d-flex my-2 my-lg-0">
+                </ul>
+                <!-- <form class="d-flex my-2 my-lg-0">
           <input class="form-control me-sm-2" type="text" placeholder="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form> -->
-      </div>
-    </div>
-  </nav>
-<header>
+            </div>
+        </div>
+    </nav>
+    <header>
