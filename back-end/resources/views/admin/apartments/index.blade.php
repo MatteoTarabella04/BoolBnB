4b82865f25c314f0eaa7a4ad55a12174f0a58089
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-07-03 15:08:11 +0200

    FIX index markup

--- back-end/resources/views/admin/apartments/index.blade.php
+++ back-end/resources/views/admin/apartments/index.blade.php
@@ -2,50 +2,44 @@
 
 @section('content')
 
-@section('content')
-
-@if(session("message"))
-<div class="alert alert-success" role="alert">
-  <strong>{{ session("message") }}</strong>
-</div>
-@endif
-
 <section>
     <div class="container">
-
+        @if(session("message"))
+        <div class="alert alert-success" role="alert">
+            <strong>{{ session("message") }}</strong>
+        </div>
+        @endif
         <h2>Elenco degli appartamenti</h2>
-        <div class="container">
-            <div class="row">
-                @forelse($apartments as $apartment)
-                <div class="col">
-                    <div class="card">
-                        <div class="card-header">{{ $apartment->name }}</div>
-                        <div class="card-image">
-                            <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}">
-                        </div>
-                        <div class="card-body">
-                            <h5><strong>Descrizione </strong></h5>
-                            <p>{{ $apartment->description }}</p>
-                            <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
-                            <p>Quest'appartamento è ancora disponibile?: {{ $apartment->visible ? "Si" : "No" }}</p>
-                        </div>
-                        <div class="card-footer">
-                            <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment) }}">Dettagli</a>
-                            <a class="btn btn-primary" href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
-                            <form class="mt-3" action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
-                                @csrf
-                                @method("delete")
-                                <button type="submit" class="btn btn-primary">Elimina</button>
-                            </form>
-                        </div>
+        <div class="row">
+            @forelse($apartments as $apartment)
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">{{ $apartment->name }}</div>
+                    <div class="card-image">
+                        <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}">
+                    </div>
+                    <div class="card-body">
+                        <h5><strong>Descrizione </strong></h5>
+                        <p>{{ $apartment->description }}</p>
+                        <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
+                        <p>Quest'appartamento è ancora disponibile?: {{ $apartment->visible ? "Si" : "No" }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment) }}">Dettagli</a>
+                        <a class="btn btn-primary" href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
+                        <form class="mt-3" action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
+                            @csrf
+                            @method("delete")
+                            <button type="submit" class="btn btn-primary">Elimina</button>
+                        </form>
                     </div>
                 </div>
-                @empty
-                <div class="col">
-                    <h3 class="text-center">Non hai ancora registrato alcun appartamento</h3>
-                </div>
-                @endforelse
             </div>
+            @empty
+            <div class="col">
+                <h3 class="text-center">Non hai ancora registrato alcun appartamento</h3>
+            </div>
+            @endforelse
         </div>
         <a class="btn btn-primary" href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a>
     </div>
