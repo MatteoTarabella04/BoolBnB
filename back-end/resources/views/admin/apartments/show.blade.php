d3bf67f8d7d4f94f2386acbd1806f13b4a9f110b
RiccardoGrande <grandericcardo1997@gmail.com>
2023-07-04 12:14:14 +0200

    fix show.blade layout

--- back-end/resources/views/admin/apartments/show.blade.php
+++ back-end/resources/views/admin/apartments/show.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-  <h2>{{ $apartment->name }}</h2>
-  <div class="image">
-  <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}">
+
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex m-auto mt-3 justify-center rounded-1">
+        <div class="image col-4">
+        <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}">
+        </div>
+        <div class="apartment_description m-1 col-8 p-2">
+            <h2>{{ $apartment->name }}</h2>
+            <p><strong>Descrizione:</strong> {{ $apartment->description }}</p>
+            <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
+            <p> <strong>Numero di stanze:</strong> {{ $apartment->rooms }}</p>
+            <p><strong>Numero di letti:</strong> {{ $apartment->beds }}</p>
+            <p><strong>Bagni:</strong> {{ $apartment->bathrooms }}</p>
+            <p><strong>Grandezza dell'appartamento:</strong> {{ $apartment->square_meters }}mq</p>
+            <p><strong>Indirizzo:</strong>{{ $apartment->address }}</p>
+            <p>Quest'appartamento è ancora disponibile?:<strong>{{ $apartment->visible ? "Si" : "No" }}</strong> </p>
+            <a class="btn btn-primary" href="{{ route('admin.apartments.index') }}">Torna all'elenco degli appartamenti</a>
+            <a class="btn btn-primary" href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
+            <form class="mt-3 d-inline-block" action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
+              @csrf
+              @method("delete")
+              <button class="btn btn-danger" type="submit">Elimina</button>
+            </form>
+        </div>
+        
+      </div>
+    </div>
+    @endsection
   </div>
-  <div>
-      <p><strong>Descrizione:</strong> {{ $apartment->description }}</p>
-      <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
-      <p>Numero di stanze: {{ $apartment->rooms }}</p>
-      <p>Numero di letti: {{ $apartment->beds }}</p>
-      <p>Bagni: {{ $apartment->bathrooms }}</p>
-      <p>Grandezza dell'appartamento: {{ $apartment->square_meters }}mq</p>
-      <p>Indirizzo:{{ $apartment->address }}</p>
-      <p>Quest'appartamento è ancora disponibile?: {{ $apartment->visible ? "Si" : "No" }}</p>
-  </div>
-  <div>
-      <a class="btn btn-primary" href="{{ route('admin.apartments.index') }}">Torna all'elenco degli appartamenti</a>
-      <a class="btn btn-primary" href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
-      <form class="mt-3" action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
-        @csrf
-        @method("delete")
-        <button class="btn btn-primary" type="submit">Elimina</button>
-      </form>
-  </div>
-</div>
-@endsection
+</section>
