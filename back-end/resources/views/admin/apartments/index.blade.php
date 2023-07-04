ec24499f69de19a350a4907582987afdb62c74d3
DamianoCasolari <damianocasolari@gmail.com>
2023-07-04 22:41:58 +0200

    big changes to view/index and view/show

--- back-end/resources/views/admin/apartments/index.blade.php
+++ back-end/resources/views/admin/apartments/index.blade.php
@@ -1,49 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+    <section>
+        <div class="container">
+            @if (session('message'))
+                <div class="alert alert-success" role="alert">
+                    <strong>{{ session('message') }}</strong>
+                </div>
+            @endif
+            <h2 class="mt-3 fw-bold">Elenco degli appartamenti</h2>
+            <div class="row">
+                @forelse($apartments as $apartment)
+                    <div class="col-12 d-flex align-items-center flex-wrap rounded-1 m-1">
+                        <div class="col-12 col-sm-6 col-md-4 p-3">
+                            <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}"
+                                class="w-100 rounded-3 ">
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-8 p-3 d-flex flex-column justify-content-center">
+                            <h5><strong>Appartamento </strong></h5>
+                            <div class="">{{ $apartment->name }}</div>
 
-<section>
-    <div class="container">
-        @if(session("message"))
-        <div class="alert alert-success" role="alert">
-            <strong>{{ session("message") }}</strong>
-        </div>
-        @endif
-        <h2>Elenco degli appartamenti</h2>
-        <div class="row">
-            @forelse($apartments as $apartment)
-            <div class="col-12 d-flex  rounded-1 m-1">
-                <div class="col-4 p-1">
-                    <img src="{{ asset('storage/' . $apartment->image) }}" alt="Immagine {{ $apartment->name }}">
-                </div>
-                <div class="col-8 p-1">
-                    <h5><strong>Appartamento </strong></h5>
-                    <div class="">{{ $apartment->name }}</div>
-                    
-                    <div class="">
-                        <h5><strong>Descrizione </strong></h5>
-                        <p>{{ $apartment->description }}</p>
-                        <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
-                        <p>Quest'appartamento è ancora disponibile?: <strong>{{ $apartment->visible ? "Si" : "No" }}</strong> </p>
+                            <div class="">
+                                <h5><strong>Descrizione </strong></h5>
+                                <p>{{ $apartment->description }}</p>
+                                <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
+                                <p>Quest'appartamento è ancora disponibile?:
+                                    <strong>{{ $apartment->visible ? 'Si' : 'No' }}</strong>
+                                </p>
+                            </div>
+                            <div class="index_buttons">
+                                <a class="btn btn-primary btn-sm"
+                                    href="{{ route('admin.apartments.show', $apartment) }}">Dettagli</a>
+                                <a class="btn btn-primary btn-sm"
+                                    href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
+                                <form class="mt-3 d-inline-block"
+                                    action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div class="index_buttons">
-                        <a class="btn btn-primary btn-sm" href="{{ route('admin.apartments.show', $apartment) }}">Dettagli</a>
-                        <a class="btn btn-primary btn-sm" href="{{ route('admin.apartments.edit', $apartment) }}">Modifica</a>
-                        <form class="mt-3 d-inline-block" action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
-                            @csrf
-                            @method("delete")
-                            <button type="submit" class="btn btn-danger btn-sm">Elimina</button>
-                        </form>
+                @empty
+                    <div class="col">
+                        <h3 class="text-center">Non hai ancora registrato alcun appartamento</h3>
                     </div>
-                </div>
+                @endforelse
             </div>
-            @empty
-            <div class="col">
-                <h3 class="text-center">Non hai ancora registrato alcun appartamento</h3>
-            </div>
-            @endforelse
+            <a class="btn btn-primary" href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a>
         </div>
-        <a class="btn btn-primary" href="{{ route('admin.apartments.create') }}">Aggiungi appartamento</a>
-    </div>
-</section>
+    </section>
 @endsection
--- back-end/resources/views/partials/AppHeader.blade.php
+++ back-end/resources/views/partials/AppHeader.blade.php
@@ -1,7 +1,8 @@
 <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <nav class="navbar navbar-expand-md navbar-light bg-light position-sticky top-0 z_index_1000">
         <div class="container">
-            <a class="text-decoration-none fw-bold fs-2 text-dark" href="{{ route('admin.apartments.index') }}">BoolBnB</a>
+            <a class="text-decoration-none fw-bold fs-2 text-dark"
+                href="{{ route('admin.apartments.index') }}">BoolBnB</a>
             {{-- config('app.name', 'Laravel') --}}
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
@@ -53,4 +54,4 @@
             </div>
         </div>
     </nav>
-<header>
+    <header>
