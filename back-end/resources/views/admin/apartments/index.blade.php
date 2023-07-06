a2f0974abdd702a56d0ce3de9be77905c08e51fe
DamianoCasolari <damianocasolari@gmail.com>
2023-07-06 02:06:27 +0200

    add style to create/show/index/edit elements

--- back-end/resources/views/admin/apartments/index.blade.php
+++ back-end/resources/views/admin/apartments/index.blade.php
@@ -5,8 +5,9 @@
         <div class="bg_double_show body_minus_header_block"></div>
         <div class="container">
             @if (session('message'))
-                <div class="alert alert-success" role="alert">
-                    <strong>{{ session('message') }}</strong>
+                <div class="alert alert-success alert-dismissible fade show mt-4" role="alert">
+                    <strong>{{ session('message') }}</strong> You should check in on some of those fields below.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
             <h1 class="pt-3 fw-bold text-center">I tuoi annunci</h1>
@@ -19,7 +20,7 @@
                                 d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                         </svg>
                     </span>
-                    <span class="text_from_left">Inserisci annuncio</span></a>
+                    <span class="text_from_left" style="margin: 10px 0 5px 0;">Inserisci annuncio</span></a>
             </div>
             <div class="row">
                 @forelse($apartments as $apartment)
@@ -29,19 +30,22 @@
                                 class="w-100 rounded-3 strong_shadow">
                         </div>
                         <div class="col-12 col-sm-6 col-md-8 p-3 d-flex flex-column justify-content-center">
-                            <h5><strong>Appartamento </strong></h5>
-                            <div>{{ $apartment->name }}</div>
+                            <h3><strong>{{ $apartment->name }} </strong></h3>
                             <div>
-                                <h5><strong>Descrizione </strong></h5>
-                                <p>{{ $apartment->description }}</p>
+                                <h6><strong>{{ $apartment->apartment_type->name }}</strong></h6>
                                 <p><strong>Prezzo per notte:</strong> {{ $apartment->price_per_night }}€</p>
-                                <p>Quest'appartamento è ancora disponibile?:
-                                    <strong>{{ $apartment->visible ? 'Si' : 'No' }}</strong>
+                                <p class="fw-bold {{ $apartment->visible ? 'text-success' : 'text-danger' }}">
+                                    @if ($apartment->visible)
+                                        Disponibile &#x2713;
+                                    @else
+                                        Non Disponibile &#x2717;
+                                    @endif
                                 </p>
                             </div>
                             <div class="index_buttons">
-                                <div class="d-flex justify-content-start flex-wrap">
-                                    <a class="btn me-2 btn-light strong_shadow"
+                                <div
+                                    class="d-flex justify-content-start justify-content-sm-center justify-content-md-start flex-wrap flex-sm-column flex-md-row">
+                                    <a class="btn me-2 my-2 btn-light strong_shadow"
                                         href="{{ route('admin.apartments.show', $apartment) }}">
                                         <span class="icon">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -53,7 +57,7 @@
                                         </span>
                                         <span class="text_from_left">Dettagli</span>
                                     </a>
-                                    <a class="btn me-2 btn-light strong_shadow"
+                                    <a class="btn me-2 my-2  btn-light strong_shadow"
                                         href="{{ route('admin.apartments.edit', $apartment) }}">
                                         <span class="icon">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -64,10 +68,11 @@
                                         </span>
                                         <span class="text_from_left">Modifica</span>
                                     </a>
-                                    <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
+                                    <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="post"
+                                        class="text-center ">
                                         @csrf
                                         @method('delete')
-                                        <button class="btn btn-dark strong_shadow" type="submit">
+                                        <button class="btn btn-dark my-2 strong_shadow width_minus_8" type="submit">
                                             <span class="icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                                     fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
@@ -82,6 +87,7 @@
                             </div>
                         </div>
                     </div>
+                    <hr>
                 @empty
                     <div class="col">
                         <h3 class="text-center">Non hai ancora registrato alcun appartamento</h3>
