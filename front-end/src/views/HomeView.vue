bd947ff6df396ec1071369af3b7e5d9569b10cd9
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-07-13 01:01:20 +0200

    CHANGE sponsorized layouts

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -195,55 +195,39 @@ export default {
 
         <div v-if="store.apartments.length > 0" class="container">
 
-            <div v-if="store.sponsorizedApartmentsPresent">
-                <h1 class="text-center my-5">Annunci in evidenza</h1>
-                <div class="d-flex flex-wrap justify-content-center">
-                    <template v-for="apartment in store.apartments">
-                        <div class="col-12 col-sm-6 col-lg-4 col-xxl-3 sponsored_apartment" v-if="store.checkIfSponsorized(apartment)">
-                            <router-link
-                                :to="{ name: 'singleApartment', params: { slug: apartment.slug } }"
-                                class="text-decoration-none">
-                                <div class="post_card text-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor"
-                        class="bi bi-bookmark-star-fill position-absolute top-0 start-0 ms-1 text-warning"
-                        viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                            d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z" />
-                                    </svg>
-                                    <DrawingPin></DrawingPin>
-                                    <img :src="store.getImagePath(apartment.image)"
-                                        class="card-img-top moving_image pointer card_shadow h-100"
-                                        :alt="apartment.name + ' image'">
-                                    <div class="position-absolute text-white bg_purple border_radius_30 px-2 m-1"> {{
-                                        Math.floor(apartment.price_per_night).toLocaleString() + " €" }}
-                                    </div>
-                                    <h2>{{ apartment.name }}</h2>
-                                    <p> {{ apartment.address }}</p>
-                                    <p> {{ apartment.description.length > 200 ?
-                                        apartment.description.slice(0, 247) +
-                                        '...' :
-                                        apartment.description }}</p>
-                                </div>
-                            </router-link>
-                        </div>
-                    </template>
-                </div>
-            </div>
-
-            <h1 class="text-center my-5">Annunci</h1>
+            <h1 class="text-center my-5">In primo piano</h1>
             <div class="d-flex flex-wrap justify-content-center">
-
+                <template v-for="apartment in store.apartments">
+                    <div class="col-12 col-sm-6 col-lg-4 col-xxl-3 sponsored_apartment" v-if="store.checkIfSponsorized(apartment)">
+                        <router-link
+                            :to="{ name: 'singleApartment', params: { slug: apartment.slug } }"
+                            class="text-decoration-none">
+                            <div class="post_card text-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-bookmark-star-fill position-absolute top-0 start-0 ms-1 text-warning" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z" />
+                                </svg>
+                                <DrawingPin></DrawingPin>
+                                <img :src="store.getImagePath(apartment.image)"
+                                    class="card-img-top moving_image pointer card_shadow h-100"
+                                    :alt="apartment.name + ' image'">
+                                <div class="position-absolute text-white bg_purple border_radius_30 px-2 m-1"> {{
+                                    Math.floor(apartment.price_per_night).toLocaleString() + " €" }}
+                                </div>
+                                <h2>{{ apartment.name }}</h2>
+                                <p> {{ apartment.address }}</p>
+                                <p> {{ apartment.description.length > 200 ?
+                                    apartment.description.slice(0, 247) +
+                                    '...' :
+                                    apartment.description }}</p>
+                            </div>
+                        </router-link>
+                    </div>
+                </template>
                 <template v-for="apartment in store.apartments">
                     <div class="col-12 col-sm-6 col-lg-4 col-xxl-3 sponsored_apartment" v-if="!store.checkIfSponsorized(apartment)">
                         <router-link :to="{ name: 'singleApartment', params: { slug: apartment.slug } }"
                             class="text-decoration-none">
                             <div class="post_card text-center">
-                                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor"
-                                    class="bi bi-bookmark-star-fill position-absolute top-0 end-0 me-2 text-warning"
-                                    viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z" />
-                                </svg> -->
                                 <DrawingPin></DrawingPin>
                                 <img :src="store.getImagePath(apartment.image)"
                                     class="card-img-top moving_image pointer card_shadow h-100"
