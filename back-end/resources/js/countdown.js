3ff45713bc02dfdcd2bd0363e0902449d0c17b34
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-07-16 01:13:45 +0200

    FIX countdown wrong calculation

--- back-end/resources/js/countdown.js
+++ back-end/resources/js/countdown.js
@@ -5,19 +5,20 @@ function startCountdown(endDate) {
 
       if (timeDiff <= 0) {
           clearInterval(countdownInterval);
+          location.reload();
       }
 
       const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       const hoursLeft = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutesLeft = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
 
-      remainingTimeEl.innerHTML = `${daysLeft} giorni, ${hoursLeft + 2} ore, ${minutesLeft} minuti`;
+      remainingTimeEl.innerHTML = `${daysLeft} giorni, ${hoursLeft} ore, ${minutesLeft} minuti`;
   }, 1000);
 }
 
 const latestExpiryDate = document.getElementById("latestExpiryDate").value;
 const remainingTimeEl = document.getElementById("remainingTime");
 if(latestExpiryDate != "") {
-  const expiryFrom1970 = new Date(latestExpiryDate).getTime();
+  const expiryFrom1970 = new Date(latestExpiryDate).getTime() + 7200000; //ADD 7200000ms TO COMPENSATE UTC TIME ON DATABASE
   startCountdown(expiryFrom1970);
 }
